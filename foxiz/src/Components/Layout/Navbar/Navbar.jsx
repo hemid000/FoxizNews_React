d26e9df271a95cba463b65fac00644c4566c74b7
hemid000 <hmid.yusifli04@mail.ru>
2023-03-22 00:09:49 +0400

    Navbar changes (politics field is added)

--- foxiz/src/Components/Layout/Navbar/Navbar.jsx
+++ foxiz/src/Components/Layout/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../Navbar/Navbar.scss";
-import { BiChevronDown } from "react-icons/bi";
-import { BsBookmark } from "react-icons/bs";
+import { BiChevronDown, BiTrendingUp } from "react-icons/bi";
+import { BsBookmark, BsClock } from "react-icons/bs";
 import { RiMore2Fill } from "react-icons/ri";
 import { MdNotificationsNone } from "react-icons/md";
 import { FiSearch } from "react-icons/fi";
@@ -35,15 +35,132 @@ const Navbar = () => {
                   Home <BiChevronDown className="down" />
                   <div className="home_hover">
                     <ul>
-                      <li>Home 2</li>
+                      <li>
+                        Home 2<div className="add_info">Hot</div>
+                      </li>
                       <li>Home 3</li>
-                      <li>Home 4</li>
+                      <li>
+                        Home 4<div className="add_info2">New</div>
+                      </li>
                       <li>Home 5</li>
                     </ul>
                   </div>
                 </li>
-                <li>
+                <li id="politics">
                   Politics <BiChevronDown className="down" />
+                  <div className="politics_hover">
+                    <div className="container">
+                      <div className="row">
+                        <div className="col-lg-4 left_side">
+                          <p>Politics</p>
+                          <div className="img">
+                            <img
+                              id="front_img"
+                              src="https://foxiz.themeruby.com/default/wp-content/uploads/sites/2/2021/09/b36-860x573.jpg"
+                              alt=""
+                            />
+                            <img
+                              id="back_img"
+                              src="https://foxiz.themeruby.com/default/wp-content/uploads/sites/2/2021/09/p12-860x573.jpg"
+                              alt=""
+                            />
+                          </div>
+
+                          <span>
+                            Politics is the art of looking for trouble, finding
+                            it everywhere, diagnosing it incorrectly and
+                            applying the wrong remedies.
+                          </span>
+                          <div className="show">
+                            <button id="btn_SHOW">Show more</button>
+                          </div>
+                        </div>
+                        <div className="col-lg-4 middle_side">
+                          <p id="topics">
+                            <BiTrendingUp style={{ color: "red" }} /> Top News
+                          </p>
+
+                          <div className="info">
+                            <div className="info_short">
+                              <img
+                                src="https://foxiz.themeruby.com/default/wp-content/uploads/sites/2/2021/08/p50-150x150.jpg"
+                                alt=""
+                              />
+                              <div className="info_right">
+                                <p>
+                                  White House Reminds Lawmakers not to Travel to
+                                  Afghanistan
+                                </p>
+                                <span>Sep 24, 2021</span>
+                              </div>
+                            </div>
+                            <div className="info_short">
+                              <img
+                                src="https://foxiz.themeruby.com/default/wp-content/uploads/sites/2/2021/08/p54-150x150.jpg"
+                                alt=""
+                              />
+                              <div className="info_right">
+                                <p>
+                                  Two Anti-Lockdown Leaders Arrested as Protests
+                                  Held Across Valinor
+                                </p>
+                                <span>Sep 24, 2021</span>
+                              </div>
+                            </div>
+                            <div className="info_short">
+                              <img
+                                src="https://foxiz.themeruby.com/default/wp-content/uploads/sites/2/2021/08/b36-150x150.jpg"
+                                alt=""
+                              />
+                              <div className="info_right">
+                                <p>
+                                  National Day Rally 2023: Sacrifice, Effort
+                                  Needed to Preserve Harmony
+                                </p>
+                                <span>Oct 5, 2021</span>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="col-lg-4 right_side">
+                          <p id="topics">
+                            <BsClock style={{ color: "red" }} /> Latest News
+                          </p>
+                          <div className="info">
+                            <div className="info_right">
+                              <p>
+                                Bad Credit Shouldn’t Affect Health Insurance,
+                                Experts Say
+                              </p>
+                              <span>Nov 2, 2021</span>
+                            </div>
+
+                            <div className="info_right">
+                              <p>
+                                What Is Women’s Equality Day and Why Is It
+                                Celebrated?
+                              </p>
+                              <span>Sep 24, 2021</span>
+                            </div>
+
+                            <div className="info_right">
+                              <p>
+                                New Census Data Will Shake Up Alabama Politics
+                              </p>
+                              <span>Sep 25, 2021</span>
+                            </div>
+                            <div className="info_right">
+                              <p>
+                                The States Braces for Protests Over New COVID
+                                Rules
+                              </p>
+                              <span>Nov 3, 2021</span>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </li>
                 <li>
                   Technology <BiChevronDown className="down" />

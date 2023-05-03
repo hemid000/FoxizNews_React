0bee9fb5fa6646ec7573b4f0b1825965cf81c7e5
hemid000 <hmid.yusifli04@mail.ru>
2023-05-03 20:22:11 +0400

    Bookmarks changes

--- foxiz/src/Components/Mock/BookmarkMock.jsx
+++ foxiz/src/Components/Mock/BookmarkMock.jsx
@@ -32,3 +32,132 @@ export const BookmarkCatgeory = [
     info: "14 Articles",
   },
 ];
+
+export const Book_news = [
+  {
+    id: Math.floor(Math.random() * 100),
+    imgSrc1:
+      "https://foxiz.themeruby.com/default/wp-content/uploads/sites/2/2021/09/av2-60x60.jpg",
+
+    info: "How My Phone’s Most Annoying Feature Saved My Life",
+    infospan:
+      "TECHNOLOGY is the art of looking for trouble, finding it everywhere, diagnosing…",
+    name: "Jacob",
+    time: "Sep 19, 2021",
+    type: "TECHNOLOGY",
+    imgSrc2:
+      "https://foxiz.themeruby.com/default/wp-content/uploads/sites/2/2021/09/t54-1-420x280.jpg",
+  },
+  {
+    id: Math.floor(Math.random() * 100),
+    imgSrc1:
+      "https://foxiz.themeruby.com/default/wp-content/uploads/sites/2/2021/09/av2-60x60.jpg",
+
+    info: "Personal loan Interest Rates Rise, Still Much Lower than Same Time Last Year",
+    infospan:
+      "TECHNOLOGY is the art of looking for trouble, finding it everywhere, diagnosing…",
+    name: "Jacob",
+    time: " Sep 9, 2021",
+    type: "BUSINESS",
+    imgSrc2:
+      "https://foxiz.themeruby.com/default/wp-content/uploads/sites/2/2021/08/b9-420x280.jpg",
+  },
+  {
+    id: Math.floor(Math.random() * 100),
+    imgSrc1:
+      "https://foxiz.themeruby.com/default/wp-content/uploads/sites/2/2021/09/av2-60x60.jpg",
+
+    info: "Corsair HS80 RGB Wireless Gaming Headset Review",
+    infospan:
+      "TECHNOLOGY is the art of looking for trouble, finding it everywhere, diagnosing…",
+    name: "Jacob",
+    time: "Sep 19, 2021",
+    type: "TECHNOLOGY",
+    imgSrc2:
+      "https://foxiz.themeruby.com/default/wp-content/uploads/sites/2/2021/09/t102-420x280.jpg",
+  },
+  {
+    id: Math.floor(Math.random() * 100),
+    imgSrc1:
+      "https://foxiz.themeruby.com/default/wp-content/uploads/sites/2/2021/09/av2-60x60.jpg",
+
+    info: "Self-Driving Cars: Everything You Need to Know",
+    infospan:
+      "TECHNOLOGY is the art of looking for trouble, finding it everywhere, diagnosing…",
+    name: "Jacob",
+    time: "Sep 19, 2021",
+    type: "BUSINESS",
+    imgSrc2:
+      "https://foxiz.themeruby.com/default/wp-content/uploads/sites/2/2021/08/t13-420x280.jpg",
+  },
+  {
+    id: Math.floor(Math.random() * 100),
+    imgSrc1:
+      "https://foxiz.themeruby.com/default/wp-content/uploads/sites/2/2021/09/av2-60x60.jpg",
+
+    info: "Girls in Ocean Science Conference a First at Maritime Museum    ",
+    infospan:
+      "TECHNOLOGY is the art of looking for trouble, finding it everywhere, diagnosing…",
+    name: "Jacob",
+    time: "Sep 8, 2021",
+    type: "TECHNOLOGY",
+    imgSrc2:
+      "https://foxiz.themeruby.com/default/wp-content/uploads/sites/2/2021/08/c14-420x280.jpg",
+  },
+  {
+    id: Math.floor(Math.random() * 100),
+    imgSrc1:
+      "https://foxiz.themeruby.com/default/wp-content/uploads/sites/2/2021/09/av2-60x60.jpg",
+
+    info: "Google Must Allow Developers to Use Other Payment Systems",
+    infospan:
+      "TECHNOLOGY is the art of looking for trouble, finding it everywhere, diagnosing…",
+    name: "Jacob",
+    time: "Sep 19, 2021",
+    type: "TECHNOLOGY",
+    imgSrc2:
+      "https://foxiz.themeruby.com/default/wp-content/uploads/sites/2/2021/08/t5-420x280.jpg",
+  },
+  {
+    id: Math.floor(Math.random() * 100),
+    imgSrc1:
+      "https://foxiz.themeruby.com/default/wp-content/uploads/sites/2/2021/09/av2-60x60.jpg",
+
+    info: "People in the Northeast Could Prevent a Covid-19 by Following These Measures",
+    infospan:
+      "TECHNOLOGY is the art of looking for trouble, finding it everywhere, diagnosing…",
+    name: "Jacob",
+    time: "Sep 5, 2021",
+    type: "TECHNOLOGY",
+    imgSrc2:
+      "https://foxiz.themeruby.com/default/wp-content/uploads/sites/2/2021/08/m19-420x280.jpg",
+  },
+  {
+    id: Math.floor(Math.random() * 100),
+    imgSrc1:
+      "https://foxiz.themeruby.com/default/wp-content/uploads/sites/2/2021/09/av2-60x60.jpg",
+
+    info: "Business Casual: The Definitive Guide for Women To Be Stylish At Work",
+    infospan:
+      "TECHNOLOGY is the art of looking for trouble, finding it everywhere, diagnosing…",
+    name: "Jacob",
+    time: "Sep 4, 2021",
+    type: "TECHNOLOGY",
+    imgSrc2:
+      "https://foxiz.themeruby.com/default/wp-content/uploads/sites/2/2021/09/g4-420x280.jpg",
+  },
+  {
+    id: Math.floor(Math.random() * 100),
+    imgSrc1:
+      "https://foxiz.themeruby.com/default/wp-content/uploads/sites/2/2021/09/av2-60x60.jpg",
+
+    info: "Here Are the 4 Cheapest Electric Vehicles You Can Buy",
+    infospan:
+      "TECHNOLOGY is the art of looking for trouble, finding it everywhere, diagnosing…",
+    name: "Jacob",
+    time: "Sep 4, 2021",
+    type: "HEALTH",
+    imgSrc2:
+      "https://foxiz.themeruby.com/default/wp-content/uploads/sites/2/2021/09/m7-420x280.jpg",
+  },
+];
--- foxiz/src/Components/Pages/Bookmarks/BookmarkRecommended/BRecommend.jsx
+++ foxiz/src/Components/Pages/Bookmarks/BookmarkRecommended/BRecommend.jsx
@@ -1,7 +1,48 @@
 import React from "react";
 import "../BookmarkRecommended/BRecommend.scss";
+import { Book_news } from "../../../Mock/BookmarkMock";
+
 const BRecommend = () => {
-  return <section id="brecommend"></section>;
+  return (
+    <section id="brecommend">
+      <div className="container">
+        <div className="row mt-5 d-flex ">
+          <p id="editor_p">Recommended for You</p>
+
+          {Book_news &&
+            Book_news.map((el) => {
+              return (
+                <div key={el.id} className="col-lg-4 col-md-6 ">
+                  <div className="row align-items-center flex-column">
+                    <div className="second">
+                      <div className="img">
+                        <div className="box">
+                          <p id="line">{el.type}</p>
+                        </div>
+                        <img src={el.imgSrc2} alt="" />
+                      </div>
+                    </div>
+                    <div className="right_side">
+                      <div className="first">
+                        <p id="info">{el.info}</p>
+                        <span id="infospan">{el.infospan}</span>
+                        <div className="down_side">
+                          <div className="down_left">
+                            <img src={el.imgSrc1} alt="" />
+                            <p>{el.name}</p>
+                            <span>{el.time}</span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default BRecommend;
